2ab759510f0ec44fda5c327817a6e9961cd4c247
vipulc8 <vipul.c@simformsolutions.com>
2023-03-31 19:18:39 +0530

    Overview of UseState

---
+++ hooks/src/Components/Content.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UseStateHook from "./UseStateHook";
+const Content = () => {
+  return (
+    <div className="container mt-5">
+      <h1>React Hooks</h1>
+      <hr />
+      <UseStateHook />
+    </div>
+  );
+};
+
+export default Content;
---
+++ hooks/src/Components/UseStateHook.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function UseStateHook() {
+  return (
+    <div>
+      <h2>Usestate Hook </h2>
+      <p>
+        useState is a React Hook that lets you add a state variable to your
+        component.
+      </p>
+    </div>
+  );
+}
